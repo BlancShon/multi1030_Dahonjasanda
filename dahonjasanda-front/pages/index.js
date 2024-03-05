da0b5b7313a26a7875cd134bf8a6588de0a3f33d
BlancShon <tora15959@gmail.com>
2024-03-05 17:10:54 +0900

    HeroSection.js 컴포넌트 추가 2024-03-05 이병집
    - HeroSection.js 컴포넌트 추가

--- dahonjasanda-front/pages/index.js
+++ dahonjasanda-front/pages/index.js
@@ -10,6 +10,7 @@ import Footer from '../components/footer';
 import './globals.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createTheme, ThemeProvider, Container, Box, Typography, TextField, Button } from '@mui/material';
+import HeroSection from '../components/HeroSection';
 
 const inter = Inter({ subsets: ['latin'] })
 const defaultTheme = createTheme();
@@ -31,56 +32,10 @@ export default function Home() {
   return (
     <>
       <Header />
-      <ThemeProvider theme={defaultTheme}>
-        {/* Hero section with simulated parallax effect */}
-        <Box
-          sx={{
-            backgroundImage: 'url(/images/hero-image-v2.jpg)',
-            backgroundAttachment: 'fixed', // Simulate parallax effect
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            height: '65vh', // Adjusted to match your requirement
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'center',
-            alignItems: 'center',
-            color: '#fff',
-            pt: '5rem', // Added padding top
-            pb: '4rem', // Added padding bottom
-          }}
-        >
-          <Container maxWidth="md" sx={{ textAlign: 'center', maxWidth: '856px' }}>
-            <Typography variant="h3" component="h1" gutterBottom sx={{ mb: '3rem', px: '1rem' }}>
-              다혼자산다. 1인 가구(경제, 금융, 제태크) 솔루션 플랫폼
-            </Typography>
-            {/* Search Bar and Dropdowns simulated */}
-            <Box
-              component="form"
-              sx={{
-                mt: 3,
-                display: 'flex',
-                flexDirection: { xs: 'column', md: 'row' }, // Responsive design
-                alignItems: 'center',
-                justifyContent: 'space-between', // Distribute space
-              }}
-              noValidate
-              autoComplete="off"
-            >
-              <TextField
-                fullWidth
-                label="Search"
-                variant="outlined"
-                value={searchInput}
-                onChange={(e) => setSearchInput(e.target.value)}
-                sx={{ background: '#fff', borderRadius: 1, mb: { xs: 2, md: 0 } }}
-              />
-              <Button variant="contained" onClick={handleSearch} sx={{ width: { md: '20%' } }}>
-                Go
-              </Button>
-            </Box>
-          </Container>
-        </Box>
-      </ThemeProvider>
+      <HeroSection
+        backgroundImage="/images/hero-image-v2.jpg"
+        title="다혼자산다. 1인 가구(경제, 금융, 제태크) 솔루션 플랫폼"
+      />
       <Footer />
     </>
   )
