6c2fe9a132602134af349c4190b1021a34a32a38
Hyeon(Hena) Park <93799006+Hena1234@users.noreply.github.com>
2024-03-14 13:11:41 +0900

    2024-03-14/housing 프론트페이지 수정/박현

--- dahonjasanda-front/pages/housingFront/components/Hero/Hero.js
+++ dahonjasanda-front/pages/housingFront/components/Hero/Hero.js
@@ -1,85 +1,122 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import { useTheme } from '@mui/material/styles';
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import { alpha, useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
-import Container from 'components/Container';
+import Container from "components/Container";
 
 const Hero = () => {
   const theme = useTheme();
+  const isMd = useMediaQuery(theme.breakpoints.up("md"), {
+    defaultMatches: true,
+  });
+
   return (
     <Box
-      position={'relative'}
+      position={"relative"}
       sx={{
-        backgroundImage:
-          'url("https://atriarealestate.com.au/wp-content/uploads/2023/09/Copy-of-10-Companies-That-Hire-for-Remote-Real-Estate-Jobs.jpeg")',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        marginTop: -13,
-        paddingTop: 13,
-        '&:after': {
-          position: 'absolute',
-          content: '" "',
-          width: '100%',
-          height: '100%',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          zIndex: 1,
-          background: '#161c2d',
-          opacity: 0.6,
-        },
+        backgroundColor: "skyblue",
       }}
     >
-      <Container
-        zIndex={3}
-        position={'relative'}
-        minHeight={{ xs: 300, sm: 400, md: 600 }}
-        maxHeight={600}
-        display={'flex'}
-        alignItems={'center'}
-        justifyContent={'center'}
-      >
-        <Box>
-          <Box marginBottom={2}>
-            <Typography
-              variant="h2"
-              align={'center'}
-              sx={{
-                fontWeight: 700,
-                color: theme.palette.common.white,
-              }}
+      <Container>
+        <Grid container spacing={4}>
+          <Grid item container alignItems={"center"} xs={12} md={6}>
+            <Box>
+              <Box marginBottom={2}>
+                <Typography
+                  variant="h2"
+                  color="text.primary"
+                  sx={{
+                    fontWeight: 700,
+                  }}
+                >
+                  <Typography
+                    component={"span"}
+                    variant={"inherit"}
+                    color={"white"}
+                    sx={{
+                      background: `linear-gradient(180deg, transparent 82%, ${alpha(
+                        theme.palette.secondary.main,
+                        0.3
+                      )} 0%)`,
+                    }}
+                  >
+                    다-혼자산다 청약홈
+                  </Typography>
+                </Typography>
+              </Box>
+              <Box marginBottom={3}>
+                <Typography
+                  variant="h6"
+                  component="p"
+                  color="text.secondary"
+                  sx={{ fontWeight: 400 }}
+                >
+                  오늘의 청약정보를 다혼자산다와 함께 확인해보세요!
+                </Typography>
+              </Box>
+              <Box
+                display="flex"
+                flexDirection={{ xs: "column", sm: "row" }}
+                alignItems={{ xs: "stretched", sm: "flex-start" }}
+              >
+                <Box
+                  marginTop={{ xs: 2, sm: 0 }}
+                  marginLeft={{ sm: 2 }}
+                  width={{ xs: "100%", md: "auto" }}
+                ></Box>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box
+              height={1}
+              width={1}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
             >
-              다-혼자산다 청약
-            </Typography>
-          </Box>
-          <Box>
-            <Typography
-              variant="h6"
-              align={'center'}
-              sx={{
-                color: theme.palette.common.white,
-              }}
-            >
-              새로운 시작, 새로운 집
-            </Typography>
-          </Box>
-        </Box>
+              <Box height={1} width={1} maxWidth={500}>
+                <Box
+                  component={"img"}
+                  src={
+                    "https://cdni.iconscout.com/illustration/premium/thumb/family-moving-to-new-house-4521312-3757064.png?f=webp"
+                  }
+                  width={1}
+                  height={1}
+                  sx={{
+                    filter:
+                      theme.palette.mode === "dark"
+                        ? "brightness(0.8)"
+                        : "none",
+                  }}
+                />
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
       </Container>
       <Box
-        component={'svg'}
+        component={"svg"}
         preserveAspectRatio="none"
         xmlns="http://www.w3.org/2000/svg"
         x="0px"
         y="0px"
         viewBox="0 0 1920 100.1"
-        width={1}
-        maxHeight={120}
-        bottom={0}
-        position={'absolute'}
-        zIndex={2}
-      ></Box>
+        sx={{
+          width: "100%",
+          marginBottom: theme.spacing(-1),
+        }}
+      >
+        <path
+          fill={theme.palette.background.paper}
+          d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
+        ></path>
+      </Box>
     </Box>
   );
 };
--- dahonjasanda-front/pages/housingFront/components/PopularArticles/PopularArticles.js
+++ dahonjasanda-front/pages/housingFront/components/PopularArticles/PopularArticles.js
@@ -1,142 +1,124 @@
-import React from "react";
-import { useTheme } from "@mui/material/styles";
+import * as React from "react";
+import { styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import Button from "@mui/material/Button";
+import ButtonBase from "@mui/material/ButtonBase";
 import Typography from "@mui/material/Typography";
-import Divider from "@mui/material/Divider";
-import Card from "@mui/material/Card";
-import CardContent from "@mui/material/CardContent";
-import CardMedia from "@mui/material/CardMedia";
-import Grid from "@mui/material/Grid";
-import Avatar from "@mui/material/Avatar";
-import Pagination from "@mui/material/Pagination";
 
-const mock = [
+const images = [
   {
-    image: "https://cdn.aitimes.com/news/photo/201907/74431_30539_1043.jpg",
-    description:
-      "Sed ut perspiciatis unde omnis iste natus error sit voluptatem",
-    title: "Eiusmod tempor incididunt",
+    url: "https://media.istockphoto.com/id/1398319735/vector/charts-abstract-background.jpg?s=612x612&w=0&k=20&c=mJuLq6DBaiYLsEI60oOXegdfjWX2AcOA0wjZ9Cqr_S0=",
+    title: "청약경쟁률 확인 ",
+    width: "30%",
   },
   {
-    image: "https://assets.maccarianagency.com/backgrounds/img13.jpg",
-    description: "At vero eos et accusamus et iusto odio dignissimos ducimus",
-    title: "Sed ut perspiciatis",
+    url: "https://www.hapt.co.kr/news/photo/202305/158843_29118_1852.jpg",
+    title: "청약 당첨자 확인",
+    width: "40%",
+  },
+  {
+    url: "https://t1.daumcdn.net/thumb/R720x0.fjpg/?fname=http://t1.daumcdn.net/brunch/service/user/4Zii/image/7GxoXiPjDM5dIM67aa_IP0k5KAg.jpg",
+    title: "청약-소통방",
+    width: "30%",
   },
 ];
 
+const ImageButton = styled(ButtonBase)(({ theme }) => ({
+  position: "relative",
+  height: 200,
+  [theme.breakpoints.down("sm")]: {
+    width: "100% !important", // Overrides inline-style
+    height: 100,
+  },
+  "&:hover, &.Mui-focusVisible": {
+    zIndex: 1,
+    "& .MuiImageBackdrop-root": {
+      opacity: 0.15,
+    },
+    "& .MuiImageMarked-root": {
+      opacity: 0,
+    },
+    "& .MuiTypography-root": {
+      border: "4px solid currentColor",
+    },
+  },
+}));
+
+const ImageSrc = styled("span")({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundSize: "cover",
+  backgroundPosition: "center 40%",
+});
+
+const Image = styled("span")(({ theme }) => ({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "center",
+  color: theme.palette.common.white,
+}));
+
+const ImageBackdrop = styled("span")(({ theme }) => ({
+  position: "absolute",
+  left: 0,
+  right: 0,
+  top: 0,
+  bottom: 0,
+  backgroundColor: theme.palette.common.black,
+  opacity: 0.4,
+  transition: theme.transitions.create("opacity"),
+}));
+
+const ImageMarked = styled("span")(({ theme }) => ({
+  height: 3,
+  width: 18,
+  backgroundColor: theme.palette.common.white,
+  position: "absolute",
+  bottom: -2,
+  left: "calc(50% - 9px)",
+  transition: theme.transitions.create("opacity"),
+}));
+
 const PopularArticles = () => {
-  const theme = useTheme();
   return (
-    <Box>
-      <Box
-        display={"flex"}
-        justifyContent={"space-between"}
-        alignItems={{ xs: "flex-start", sm: "center" }}
-        flexDirection={{ xs: "column", sm: "row" }}
-        marginBottom={4}
-      >
-        <Box>
-          <Typography fontWeight={700} variant={"h6"} gutterBottom>
-            여기는 청약 지도 넣을 예정
-          </Typography>
-          <Typography color={"text.secondary"}>
-            오늘의 청약 뉴스 확인해보세요!
-          </Typography>
-        </Box>
-        <Box display="flex" marginTop={{ xs: 2, md: 0 }}>
-          <Box
-            component={Button}
-            variant="outlined"
-            color="primary"
-            size="large"
-            marginLeft={2}
-          >
-            View all
-          </Box>
-        </Box>
-      </Box>
-      <Grid container spacing={4}>
-        {mock.map((item, i) => (
-          <Grid item>
-            <Box
-              component={"a"}
-              href={""}
-              display={"block"}
-              width={1}
-              height={1}
+    <Box
+      sx={{ display: "flex", flexWrap: "wrap", minWidth: 300, width: "100%" }}
+    >
+      {images.map((image) => (
+        <ImageButton
+          focusRipple
+          key={image.title}
+          style={{
+            width: image.width,
+          }}
+        >
+          <ImageSrc style={{ backgroundImage: `url(${image.url})` }} />
+          <ImageBackdrop className="MuiImageBackdrop-root" />
+          <Image>
+            <Typography
+              component="span"
+              variant="subtitle1"
+              color="inherit"
               sx={{
-                textDecoration: "none",
-                transition: "all .2s ease-in-out",
-                "&:hover": {
-                  transform: `translateY(-${theme.spacing(1 / 2)})`,
-                },
+                position: "relative",
+                p: 4,
+                pt: 2,
+                pb: (theme) => `calc(${theme.spacing(1)} + 6px)`,
               }}
             >
-              <Box
-                component={Card}
-                width={1}
-                height={1}
-                boxShadow={4}
-                display={"flex"}
-                flexDirection={"column"}
-                sx={{ backgroundImage: "none" }}
-              >
-                <CardMedia
-                  image={item.image}
-                  title={item.title}
-                  sx={{
-                    height: { xs: 300, md: 360 },
-                    position: "relative",
-                  }}
-                >
-                  <Box
-                    component={"svg"}
-                    viewBox="0 0 2880 480"
-                    fill="none"
-                    xmlns="http://www.w3.org/2000/svg"
-                    sx={{
-                      position: "absolute",
-                      bottom: 0,
-                      color: theme.palette.background.paper,
-                      transform: "scale(2)",
-                      height: "auto",
-                      width: 1,
-                      transformOrigin: "top center",
-                    }}
-                  >
-                    <path
-                      fillRule="evenodd"
-                      clipRule="evenodd"
-                      d="M2160 0C1440 240 720 240 720 240H0v240h2880V0h-720z"
-                      fill="currentColor"
-                    />
-                  </Box>
-                </CardMedia>
-                <Box component={CardContent} position={"relative"}>
-                  <Typography variant={"h6"} gutterBottom>
-                    {item.title}
-                  </Typography>
-                  <Typography color="text.secondary">
-                    {item.description}
-                  </Typography>
-                </Box>
-                <Box flexGrow={1} />
-                <Box padding={2} display={"flex"} flexDirection={"column"}>
-                  <Box marginBottom={2}>
-                    <Divider />
-                  </Box>
-                  <Box
-                    display={"flex"}
-                    justifyContent={"space-between"}
-                    alignItems={"center"}
-                  ></Box>
-                </Box>
-              </Box>
-            </Box>
-          </Grid>
-        ))}
-      </Grid>
+              {image.title}
+              <ImageMarked className="MuiImageMarked-root" />
+            </Typography>
+          </Image>
+        </ImageButton>
+      ))}
     </Box>
   );
 };
