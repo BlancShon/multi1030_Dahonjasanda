8408117e2538be6b9d6ca7a3c0957ddf2b953254
admin <admin@localhost>
2024-03-17 13:43:38 +0900

    디비주입 구조 수정 / 2024-03-17 / 고재목
    - 수정사항 : 디비 주입 아키텍처 정리

--- dahonjasanda-db/src/main/java/com/multi/dahon/domain/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.multi.dahon.domain;
-
-public enum OrderStatus {
-    ORDER, CANCEL
-}
--- dahonjasanda-db/src/main/java/com/multi/dahon/repository/finance/SavingRepository.java
+++
@@ -1,7 +0,0 @@
-package com.multi.dahon.repository.finance;
-
-import com.multi.dahon.finance.Saving;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SavingRepository extends JpaRepository<Saving, Long> {
-}
