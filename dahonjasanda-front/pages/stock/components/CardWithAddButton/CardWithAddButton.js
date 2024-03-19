f2e7130fed8ad8bafc4d5dc4a42a67ec8ad80589
loveymiracle <thecyon@naver.com>
2024-03-19 16:58:57 +0900

    Stock 프런트 수정 / 2024-03-19 16:57 / 윤성은
    1. 작업일 : 2024-03-19 16:57
    2. 작업자 : 윤성은
    3. 작업내용 :
     - 프런트 피드백 반영
     - 지수 정보 추가
     - 히어로 이미지 height 축소
     - 탭간격 수정

--- dahonjasanda-front/pages/stock/components/CardWithAddButton/CardWithAddButton.js
+++ dahonjasanda-front/pages/stock/components/CardWithAddButton/CardWithAddButton.js
@@ -22,15 +22,22 @@ export const graph1 = [2652, 2625, 2647, 2668, 2664, 2653, 2658, 2680, 2649, 261
                       2620, 2650, 2620, 2610, 2576, 2591, 2615, 2542, 2497, 2499,
                       2501, 2479, 2470, 2470, 2479, 2464, 2473, 2440, 2436, 2498,
                       2526, 2525, 2540, 2542, 2561, 2568];
+export const graph2 = [1814.19, 1804.95, 1841.44, 1824.93, 1815.73, 1802.17, 1817.69, 1792.35, 1789.84, 1795.11,
+                      1813.93, 1785.57, 1792.85, 1769.76, 1783.06, 1802.13, 1796.36, 1786.58, 1790.09,
+                      1812.42, 1785.82, 1767.5, 1769.26, 1793.1, 1773.78, 1770.26, 1746.92, 1777.04, 1717.31,
+                      1717.31, 1691.46, 1699.99, 1702.76, 1682.18, 1681.67, 1680.67, 1688.7];
 
 const CardWithAddButton = () => {
   const theme = useTheme();
   const pvalue = graph1[graph1.length -1];
   const dvalue = graph[graph.length -1];
+  const prvalue = graph2[graph.length -1];
   const pchangeValue = graph1[graph1.length -2] - pvalue;
   const dchangeValue = graph[graph.length -2] - dvalue;
+  const prchangeValue = graph2[graph.length -2] - prvalue;
   const prate = ((pchangeValue / pvalue) * 100).toFixed(2)
   const drate = ((dchangeValue / dvalue) * 100).toFixed(2)
+  const prrate = ((prchangeValue / prvalue) * 100).toFixed(2)
   // const isNegative = pchangeValue.charAt(0) === '-';
   // const isNegative1 = dchangeValue.charAt(0) === '-';
 
@@ -38,7 +45,7 @@ const CardWithAddButton = () => {
     <Box bgcolor={'alternate.main'}>
       <Container>
         <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
-          <Card sx={{ p: { xs: 4, md: 4 }, height: '100%', width: '45%' }}>
+          <Card sx={{ p: { xs: 4, md: 4 }, height: '100%', width: '45%', border: '1px solid #ccc' }}>
             <Box
               display={'flex'}
               flexDirection={{ xs: 'column', sm: 'row' }}
@@ -64,7 +71,7 @@ const CardWithAddButton = () => {
               </Box>
             </Box>
           </Card>
-          <Card sx={{ p: { xs: 4, md: 4 }, height: '100%', width: '45%' }}>
+          <Card sx={{ p: { xs: 4, md: 4 }, height: '100%', width: '45%', border: '1px solid #ccc' }}>
             <Box
               display={'flex'}
               flexDirection={{ xs: 'column', sm: 'row' }}
@@ -85,6 +92,33 @@ const CardWithAddButton = () => {
                   {drate}% {dchangeValue}p
                 </Typography>
                 <IndexGraph data={graph1} height={100}/>
+                <Typography variant={'h6'} fontWeight={100} sx={{ textAlign: 'right'}}>
+                  03.06 실시간
+                </Typography>
+              </Box>
+            </Box>
+          </Card>
+          <Card sx={{ p: { xs: 4, md: 4 }, height: '100%', width: '45%', border: '1px solid #ccc' }}>
+            <Box
+              display={'flex'}
+              flexDirection={{ xs: 'column', sm: 'row' }}
+              flex={'1 1 100%'}
+              justifyContent={{ sm: 'space-between' }}
+              alignItems={{ xs: 'flex-start', sm: 'center' }}
+              marginBottom={4}
+              height={'100%'}
+            >
+              <Box marginBottom={{ xs: 2, sm: 0 }} flex={'1 1 100%'} >
+                <Typography variant={'h3'} fontWeight={400}>
+                  코스피 선물
+                </Typography>
+                <Typography variant={'h4'} color={dchangeValue >= 0 ? 'red' : 'blue'}>
+                  {prvalue}
+                </Typography>
+                <Typography variant={'h6'} color={dchangeValue >= 0 ? 'red' : 'blue'}>
+                  {prrate}% {prchangeValue}p
+                </Typography>
+                <IndexGraph data={graph2} height={100}/>
                 <Typography variant={'h6'} fontWeight={100} sx={{ textAlign: 'right'}}>
                   03.06 실시간
                 </Typography>
--- dahonjasanda-front/pages/stock/components/IndexGraph/indexgraph.js
+++ dahonjasanda-front/pages/stock/components/IndexGraph/indexgraph.js
@@ -1,7 +1,12 @@
+import { Typography } from '@mui/material';
 import React from 'react';
 import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 const IndexGraph = ({ data }) => {
+  if (!data || data.length === 0) {
+    return <Typography>로딩중..</Typography>;
+  }
+  
   const lastValue = data[data.length - 1];
   const prevValue = data[data.length - 2];
   const difference = lastValue - prevValue;
