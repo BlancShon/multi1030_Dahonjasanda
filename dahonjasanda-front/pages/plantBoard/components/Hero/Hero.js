ef4eead4e4fd6a24d1359991c1aae8ddf03bc9dc
Hyeon(Hena) Park <93799006+Hena1234@users.noreply.github.com>
2024-03-28 17:01:42 +0900

    동식물hero 이미지변경/박현

--- dahonjasanda-front/pages/plantBoard/components/Hero/Hero.js
+++ dahonjasanda-front/pages/plantBoard/components/Hero/Hero.js
@@ -1,89 +1,122 @@
-import React from 'react';
-import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
-import { useTheme } from '@mui/material/styles';
+/* eslint-disable react/no-unescaped-entities */
+import React from "react";
+import { alpha, useTheme } from "@mui/material/styles";
+import useMediaQuery from "@mui/material/useMediaQuery";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
-import Container from 'components/Container';
-// 수정 사항 없음
+import Container from "components/Container";
+
 const Hero = () => {
   const theme = useTheme();
+  const isMd = useMediaQuery(theme.breakpoints.up("md"), {
+    defaultMatches: true,
+  });
+
   return (
     <Box
-      position={'relative'}
+      position={"relative"}
       sx={{
-        backgroundImage:
-          'url("https://kogaswebzine.com/img/vol27/sub_title_img3-3.jpg")',
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        marginTop: -13,
-        paddingTop: 13,
-        '&:after': {
-          position: 'absolute',
-          content: '" "',
-          width: '100%',
-          height: '100%',
-          top: 0,
-          right: 0,
-          bottom: 0,
-          left: 0,
-          zIndex: 1,
-          background: '#161c2d',
-          opacity: 0.6,
-        },
+        backgroundColor: "lightgreen",
       }}
     >
-      <Container
-        zIndex={3}
-        position={'relative'}
-        minHeight={{ xs: 300, sm: 400, md: 600 }}
-        maxHeight={600}
-        display={'flex'}
-        alignItems={'center'}
-        justifyContent={'center'}
-      >
-        <Box>
-          <Box marginBottom={2}>
-            <Typography
-              variant="h2"
-              align={'center'}
-              sx={{
-                fontWeight: 700,
-                color: theme.palette.common.white,
-              }}
+      <Container>
+        <Grid container spacing={4}>
+          <Grid item container alignItems={"center"} xs={12} md={6}>
+            <Box>
+              <Box marginBottom={2}>
+                <Typography
+                  variant="h2"
+                  color="text.primary"
+                  sx={{
+                    fontWeight: 700,
+                  }}
+                >
+                  <Typography
+                    component={"span"}
+                    variant={"inherit"}
+                    color={"black"}
+                    sx={{
+                      background: `linear-gradient(180deg, transparent 82%, ${alpha(
+                        theme.palette.secondary.main,
+                        0.3
+                      )} 0%)`,
+                      fontSize: "1.13em", // 폰트 크기 키움
+                    }}
+                  >
+                    반려-게시판
+                  </Typography>
+                </Typography>
+              </Box>
+              <Box marginBottom={3}>
+                <Typography
+                  variant="h6"
+                  component="p"
+                  color="text.secondary"
+                  sx={{ fontWeight: 700, fontSize: "1.8em" }}
+                >
+                  당신의 반려 식물/동물 <br></br>사진을 공유하고 자랑해보세요!
+                </Typography>
+              </Box>
+              <Box
+                display="flex"
+                flexDirection={{ xs: "column", sm: "row" }}
+                alignItems={{ xs: "stretched", sm: "flex-start" }}
+              >
+                <Box
+                  marginTop={{ xs: 2, sm: 0 }}
+                  marginLeft={{ sm: 2 }}
+                  width={{ xs: "100%", md: "auto" }}
+                ></Box>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={12} md={6}>
+            <Box
+              height={1}
+              width={1}
+              display={"flex"}
+              justifyContent={"center"}
+              alignItems={"center"}
             >
-              1인 가족들의 소통 공간
-            </Typography>
-          </Box>
-          <Box>
-            <Typography
-              variant="h6"
-              align={'center'}
-              sx={{
-                color: theme.palette.common.white,
-              }}
-            >
-              공지 사항 및 이웃 주민들과의 소통의 공간입니다.
-            </Typography>
-          </Box>
-        </Box>
+              <Box height={1} width={1} maxWidth={660}>
+                <Box
+                  component={"img"}
+                  src={
+                    "https://cdni.iconscout.com/illustration/premium/thumb/female-flower-shop-owner-4897150-4079421.png?f=webp"
+                  }
+                  width={1}
+                  height={1}
+                  sx={{
+                    filter:
+                      theme.palette.mode === "dark"
+                        ? "brightness(0.8)"
+                        : "none",
+                  }}
+                />
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
       </Container>
       <Box
-        component={'svg'}
+        component={"svg"}
         preserveAspectRatio="none"
         xmlns="http://www.w3.org/2000/svg"
         x="0px"
         y="0px"
         viewBox="0 0 1920 100.1"
-        width={1}
-        maxHeight={120}
-        bottom={0}
-        position={'absolute'}
-        zIndex={2}
+        sx={{
+          width: "100%",
+          marginBottom: theme.spacing(-1),
+        }}
       >
-        {/* <path
-          fill={theme.palette.alternate.main}
+        <path
+          fill={theme.palette.background.paper}
           d="M0,0c0,0,934.4,93.4,1920,0v100.1H0L0,0z"
-        ></path> */}
+        ></path>
       </Box>
     </Box>
   );
